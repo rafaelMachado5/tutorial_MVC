4af624e992cf3bb270e4989671ffe0b81b628a71
rafaelMachado5 <rafael.c.machado92@gmail.com>
2018-10-14 19:02:53 +0100

    Level 2

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -55,8 +55,8 @@ namespace WebApplication1
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
